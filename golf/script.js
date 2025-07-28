43ae290a57cfa5d57d1f34537c0670f47bd3bb16
foroIA <113232828+foroIA@users.noreply.github.com>
2025-07-28 19:01:27 +0200

    Add files via upload

--- golf/script.js
+++ golf/script.js
@@ -1,5 +1,3 @@
-//const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTod6BGiBA--DmvY8tktdr1FvG_NE6mEXdJDvUWGdqH8pkA_YqFa-Jy2BCmpj3DkCnjlxJ0lL_L9moT/pubhtml';
-
 google.charts.load('current', {'packages':['table']});
 google.charts.setOnLoadCallback(drawTable);
 
@@ -16,22 +14,6 @@ function drawTable() {
     }
 
     const data = response.getDataTable();
-
-    // Calculate rank based on Total column (assuming last column is Total)
-    const totalIndex = data.getNumberOfColumns() - 1;
-    const rows = [];
-    for (let i = 0; i < data.getNumberOfRows(); i++) {
-      rows.push([i, data.getValue(i, totalIndex)]);
-    }
-    rows.sort((a, b) => a[1] - b[1]);
-    const rankMap = new Map(rows.map((r, i) => [r[0], i + 1]));
-
-    // Add Rank column
-    data.addColumn('number', 'Rank');
-    for (let i = 0; i < data.getNumberOfRows(); i++) {
-      data.setValue(i, data.getNumberOfColumns() - 1, rankMap.get(i));
-    }
-
     const table = new google.visualization.Table(document.getElementById('scoreboard'));
     table.draw(data, {showRowNumber: false, width: '100%', height: 'auto'});
   });
@@ -39,4 +21,3 @@ function drawTable() {
 
 // Auto refresh every 60 seconds
 setInterval(drawTable, 60000);
-
